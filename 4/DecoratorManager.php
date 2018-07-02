d0667be88e0f25e1ab9e60c01962469d3f3a2153
Max <bogatirev.maxim@yandex.ru>
2018-07-02 14:34:42 +0700

    4 task

---
+++ 4/DecoratorManager.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace src\Decorator;
+
+use DateTime;
+use Exception;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\LoggerInterface;
+use src\Integration\DataProvider;
+
+class DecoratorManager extends DataProvider
+{
+    public $cache;
+    public $logger;
+
+    /**
+     * @param string $host
+     * @param string $user
+     * @param string $password
+     * @param CacheItemPoolInterface $cache
+     * @param LoggerInterface $logger
+     */
+    public function __construct($host, $user, $password, CacheItemPoolInterface $cache, LoggerInterface $logger)
+    {
+        parent​::__construct($host, $user, $password);
+        $this->cache = $cache;
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResponse(array $input)
+    {
+        try {
+            $cacheKey = $this->getCacheKey($input);
+            $cacheItem = $this->cache->getItem($cacheKey);
+
+            if ($cacheItem->isHit()) {
+                return $cacheItem->get();
+            }
+
+            $result = parent​::get($input);
+            $cacheItem
+                ->set($result)
+                ->expiresAt(
+                    (new DateTime())->modify('+1 day')
+                );
+
+            return $result;
+
+        } catch (Exception $e) {
+            $this->logger->critical($e->getMessage());
+        }
+
+        return [];
+    }
+
+    public function getCacheKey(array $input)
+    {
+        return json_encode($input);
+    }
+}
